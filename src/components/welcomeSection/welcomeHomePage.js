9b34be90ff2f3ced4bfc65817a34e02b4de15398
Florian de Ghellinck <f.deghellinck@gmail.com>
2022-12-20 11:20:03 +0100

    fix:cursor

--- src/components/welcomeSection/welcomeHomePage.js
+++ src/components/welcomeSection/welcomeHomePage.js
@@ -27,7 +27,7 @@ const WelcomePage = ({ openModal, setOpenModal }) => {
               }}
               className='absolute w-full top-0h-screen wrapper col-start-5 md:col-start-7 row-start-2  leading-none'
             >
-              <h1 className='relative font-poppins font-extrabold md:text-5xl lg:text-7xl text-4xl text-gray-800 '>
+              <h1 className='relative font-poppins font-extrabold md:text-5xl lg:text-7xl text-4xl text-gray-800 hover:cursor-pointer'>
                 <span>HI, I'M FLORIAN </span> <br />
                 <span> A FULL STACK</span> <br />
                 <span className='text-transparent bg-clip-text bg-gradient-to-r from-amber-400 to-rose-600'>
