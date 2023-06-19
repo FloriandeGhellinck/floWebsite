250042e6cafc605fd405ef87e1322231708982f7
Florian de Ghellinck <f.deghellinck@gmail.com>
2023-06-19 15:05:21 +0200

    fix: paddings

--- src/components/welcomeSection/welcomeProjectsPage.tsx
+++ src/components/welcomeSection/welcomeProjectsPage.tsx
@@ -5,9 +5,9 @@ import { FC } from "react";
 const ProjectsHomePage: FC = () => {
   return (
     <>
-      <div className="h-screen w-screen p-5 ">
+      <div className="h-screen w-screen p-5 relative">
         <div className="opacity-100 h-full w-full">
-          <Image src="/projects-pic.png" className="object-cover" alt="Developer Picture" layout="fill" />
+          <Image src="/projects-pic.png" className="object-cover " alt="Developer Picture" layout="fill" />
           <div className="bg-transparent h-full w-full p-5 relative border-8 border-white grid grid-cols-12 grid-rows-6">
             <div className="absolute w-full top-0h-screen wrapper col-start-3 sm:col-start-5 md:col-start-7 row-start-2 leading-none">
               <h1 className="relative font-poppins font-extrabold md:text-4xl lg:text-6xl text-4xl  ">
